f6849fc360e6d3fb982351f11f5d02f531e8aa73
FIfa Pns <u5713074@au.edu>
2017-08-31 11:35:55 +0700

    routing hero

--- Angular2/angular-tour-of-heroes/src/app/app.component.ts
+++ Angular2/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { selector } from 'rxjs/operator/publish';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-my-app',
+  template: `
+  <h1>{{title}}</h1>
+  <a routerLink="/heroes">Heroes</a>
+  <router-outlet></router-outlet>
+  `
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
 }
--- Angular2/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular2/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,16 +1,43 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+import { HeroesComponent } from './heroes.component';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './Dashboard.component';
+
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+    ])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
